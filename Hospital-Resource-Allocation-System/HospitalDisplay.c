7d61be481ae19bf66a9d99d2b05a0c50dab527df
Thanuka <thanukathathsara20@gmail.com>
2026-09-17 11:18:25 +0530

    Add main menu

---
+++ Hospital-Resource-Allocation-System/HospitalDisplay.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void printMenu (){
+    printf("\n============SMART HOSPITAL MAIN MENU============\n");
+    printf("01. Register a patient and generate a bill\n");
+    printf("02. Display priority order\n");
+    printf("03. Display bed occupancy status\n");
+    printf("04. Display doctor specialties\n");
+    printf("05. Display hospital wards\n");
+    printf("06. Generate performance report\n");
+    printf("07. Exit\n");
+    printf("\n============************************============\n");
+    }
--- Hospital-Resource-Allocation-System/main.c
+++ Hospital-Resource-Allocation-System/main.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+//function prototype
+void printMenu();
 
-int main()
-{
-    printf("Hello world!\n");
+
+
+int main(){
+    int menuNum ;
+    do{
+        printMenu();
+        printf("Enter an option :");
+        scanf("%d",&menuNum);
+
+        switch (menuNum){
+            case 1: //Register a patient and generate a bill
+                break;
+            case 2: //Display priority order
+                break;
+            case 3: //Display bed occupancy status
+                break;
+            case 4: //Display doctor specialties
+                break;
+            case 5: //Display hospital wards
+                break;
+            case 6: //Generate performance report
+                break;
+            case 7:
+                printf("Exit program ,GOODBYE");
+                break;
+            default:
+                printf("Invalid number,(1-7)");
+        }
+    }while (menuNum != 7);
     return 0;
-}
+
+
+
+    }
